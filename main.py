99756cb4adc2a7a9b673ed8606973b5d1c1c1308
kun.qian <kun.qian06@gmail.com>
2018-07-10 16:36:23 +0800

    init v1

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import logging
 
 import requests
 import time
+import re
 from Semantic_Search.DocSimWrapper import get_sentence_vector, vecsim
 
 logging.basicConfig(level=logging.INFO)
 
 try:
     r = requests.get(
-        "http://ziggy-dev-ols.nprpaas.ddns.integ.dns-orange.fr/api/v1/ontologies/classes?includeFields=iri%2Cname&limit=10000")
+        "http://ziggy-dev-ols.nprpaas.ddns.integ.dns-orange.fr/api/v1/ontologies/classes?includeFields=iri%2Cname&limit=500")
     logging.info("received %s classes from thingin!" % len(r.json()))
 
 except requests.RequestException:
@@ -16,27 +17,34 @@ except requests.RequestException:
     exit()
 
 classes = r.json()
+filter_keywords = 'command|notification|functionality|function|unit|system|status|state'
+classes = [c for c in classes if len(re.findall(filter_keywords, c['data']['name'].lower())) == 0]
+# remove duplicate iri class
+classes = [dict(element) for element in set([tuple(c['data'].items()) for c in classes])]
+# print(classes)
+
 
 # method could be 1, 2, 3
 method = 2
 start = time.time()
 for c in classes:
-    vector = get_sentence_vector(c['data']['name'], method)
-    c['data']['vec'] = vector
+    vector = get_sentence_vector(c['name'], method)
+    c['vec'] = vector
 
 # print(classes)
 
-keywords = ['lunch', 'dinner', 'break', 'cooling', 'heating', 'sunny']
+keywords = ['lunch', 'dinner', 'drink', 'water', 'heating', 'sunny', 'cafe', 'temperature controller', 'tea', 'hungry',
+            'print', 'clean', 'charging']
 
 
 def get_top_similar_classes(vec, classes, n, threshold=0):
     res = []
     for c in classes:
-        class_vec = c['data']['vec']
+        class_vec = c['vec']
         sim = vecsim(vec, class_vec)
         if sim > threshold:
-            res.append({"class": c['data']['iri'],
-                        "name": c['data']['name'],
+            res.append({"class": c['iri'],
+                        "name": c['name'],
                         "similarity": sim})
 
     res.sort(key=lambda x: x['similarity'], reverse=True)
@@ -50,5 +58,5 @@ with open("mapping.txt", "w+") as f:
         keyword_vec = get_sentence_vector(keyword, method)
         similar_classes = get_top_similar_classes(keyword_vec, classes, 5)
         print(similar_classes)
-        similar_classes_str = ";".join(map(lambda x: x["name"], similar_classes))
-        f.write(keyword + " " + similar_classes_str + "\r\n")
+        similar_classes_str = ";".join(map(lambda x: x["class"], similar_classes))
+        f.write(keyword + ": " + similar_classes_str + "\r\n")
