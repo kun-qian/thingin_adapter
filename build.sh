c96e23cba778986355b2224ef451842602ca3f1f
kun.qian <kun.qian06@gmail.com>
2018-12-17 16:05:02 +0800

    add proxy for server

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ if [ ! -f build.sh ]; then
     echo "please go to the thingin adapter root folder, then execute!"
 fi
 
-docker build -t thingin_adapter:v1 .
+docker build --build-arg http_proxy=http://openwatt-proxy-np.itn.ftgroup:8080 \
+             --build-arg https_proxy=http://openwatt-proxy-np.itn.ftgroup:8080 \
+             -t thingin_adapter:v1 .
 #docker build -f Dockerfile_cn -t thingin_adapter:v1
