d807e7b45f62ac0fc1f5d01d0f6e74062cc73fd1
kun.qian <kun.qian06@gmail.com>
2018-12-17 16:38:40 +0800

    missing \

--- run.sh
+++ run.sh
@@ -29,6 +29,6 @@ docker run -d -p 8000:8000 -v $NLTK_DATA:/root/nltk_data \
                            -v $MODELS:/thingin_Adapter/Semantic_Search/models \
                            -v $CACHES:/thingin_Adapter/recommender/cache_files \
                            --mount type=bind,source=$LOG,target=/thingin_Adapter/thingin_recommender.log \
-                           --name thingin_adapter
+                           --name thingin_adapter \
                            thingin_adapter:v1
 fi
